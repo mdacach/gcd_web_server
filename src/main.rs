17b439fdce68f98ce3fa6e279faee06bfa844844
Matheus Cardoso <dacachmatheus@gmail.com>
2022-06-09 22:54:28 -0300

    Add handler for gcd submit

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 
 fn main() {
-    let server = HttpServer::new(|| App::new().route("/", web::get().to(get_index)));
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
 
     println!("Serving on http://localhost:7878...");
     server
@@ -16,9 +21,41 @@ fn get_index() -> HttpResponse {
         r#"
         <title>GCD Calculator</title>
         <form action="/gcd" method="post">
-        <input type="text" name="n"/>
-        <input type="text" name="m"/>
+        <input type="text" name="a"/>
+        <input type="text" name="b"/>
         <button type="submit">Compute GCD</button>
         </form>"#,
     )
+}
+
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    println!("got here with {} and {}", form.a, form.b);
+    if form.a == 0 && form.b == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("GCD(0, 0) is undefined.");
+    }
+
+    let response = format!(
+        "The greatest common divisor of the numbers {} and {} is <b>{}</b>\n",
+        form.a,
+        form.b,
+        gcd(form.a, form.b)
+    );
+
+    HttpResponse::Ok().content_type("text/html").body(response)
+}
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    a: u64,
+    b: u64,
+}
+
+fn gcd(a: u64, b: u64) -> u64 {
+    assert!(a != 0 || b != 0);
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
 }
